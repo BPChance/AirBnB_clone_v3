056b494e7cc769bced1017d850f6f7b3fa672b68
root <moshjerrick@gmail.com>
2024-07-15 20:00:27 +0000

    fixed 404 error handling

--- api/v1/app.py
+++ api/v1/app.py
@@ -15,6 +15,12 @@ def teardown_db(exception):
     storage.close()
 
 
+@app.errorhandler(404)
+def page_not_found(error):
+    '''Return JSON response for 404 errors'''
+    return jsonify({"error": "Not found"}), 404
+
+
 if __name__ == "__main__":
     host = os.getenv('HBNB_API_HOST', '0.0.0.0')
     port = os.getenv('HBNB_API_PORT', 5000)
