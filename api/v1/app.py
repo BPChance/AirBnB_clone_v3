ab8ad8907c1cbb1bdb3f9ccc28346b00b692890a
root <moshjerrick@gmail.com>
2024-07-15 19:58:55 +0000

    added 404 to app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 """ set up the flask app """
 
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 import os
-
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
