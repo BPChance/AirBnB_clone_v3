8bd9176ea98fc64c9e7923bdf544f7827c0829b7
BPChance <bradenpchance6@gmail.com>
2024-07-15 04:55:18 +0000

    removed methods=['GET'] from /stats route

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -16,7 +16,7 @@ from models.user import User
 def get_status():
     return jsonify({"status": "OK"})
 
-@app_views.route('/stats', methods=['GET'])
+@app_views.route('/stats')
 def get_stats():
     stats = {
         "amenities": storage.count("Amenity"),
