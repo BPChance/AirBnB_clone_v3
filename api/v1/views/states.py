22d173249779611c9a28fe4e00356e640eb3ff3e
BPChance <bradenpchance6@gmail.com>
2024-07-15 23:45:24 +0000

    pep8 in index and states

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -13,6 +13,7 @@ def get_states():
     states = storage.all(State).values()
     return jsonify([state.to_dict() for state in states])
 
+
 @app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
 def get_state(state_id):
     """ retrieves a state object """
@@ -21,7 +22,9 @@ def get_state(state_id):
         abort(404)
     return jsonify(state.to_dict())
 
-@app_views.route('/states/<state_id>', methods=['DELETE'], strict_slashes=False)
+
+@app_views.route('/states/<state_id>',
+                 methods=['DELETE'], strict_slashes=False)
 def delete_state(state_id):
     """ delete a state object """
     state = storage.get(State, state_id)
@@ -30,6 +33,7 @@ def delete_state(state_id):
     storage.delete(state)
     storage.save()
     return jsonify({}), 200
+
 
 @app_views.route('/states', methods=['POST'], strict_slashes=False)
 def create_state():
@@ -42,6 +46,7 @@ def create_state():
     new_state = State(**data)
     new_state.save()
     return jsonify(new_state.to_dict()), 201
+
 
 @app_views.route('/states/<state_id>', methods=['PUT'], srict_slashes=False)
 def update_state(state_id):
