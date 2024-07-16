fbae573da4295d02811f5c64e7c04b201cefc44f
BPChance <bradenpchance6@gmail.com>
2024-07-15 20:28:57 -0500

    reverted index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-""" create a status route """
-
-from api.v1.views import app_views
+""" create an endpoint """
 from flask import jsonify
+from api.v1.views import app_views
 from models import storage
 from models.amenity import Amenity
 from models.city import City
@@ -11,21 +10,20 @@ from models.review import Review
 from models.state import State
 from models.user import User
 
-model_classes = {"amenities": Amenity, "cities": City,
-           "places": Place, "reviews": Review, "states": State, "users": User}
-
 
 @app_views.route('/status', strict_slashes=False)
 def get_status():
-    """ return api status """
     return jsonify({"status": "OK"})
 
 
-@app_views.route('/stats', strict_slashes=False, methods=['GET'])
+@app_views.route('/stats')
 def get_stats():
-    """ return count for the different objects """
-    return_dict = {}
-    for object in model_classes:
-        object_dict = {object: storage.count(model_classes[object])}
-        return_dict.update(object_dict)
-    return jsonify(return_dict)
+    stats = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User")
+    }
+    return jsonify(stats)
