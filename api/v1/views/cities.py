df5442fb97e1d696a9aba444689c24ccdafb643d
Josh Merrick <moshjerrick@gmail.com>
2024-07-16 16:12:06 +0000

    revert back cities.py

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -4,7 +4,6 @@
 from flask import jsonify, request, abort
 from models import storage
 from models.city import City
-from models.state import State
 from api.v1.views import app_views
 
 
@@ -26,20 +25,17 @@ def get_city(city_id):
 
 @app_views.route('/cities/<city_id>', methods=['DELETE'], strict_slashes=False)
 def delete_city(city_id):
-    """ deletes a city object """
+    """ delete a city object """
     city = storage.get(City, city_id)
-    if city is None:
+    abort(404)
     storage.delete(city)
     storage.save()
     return jsonify({}), 200
 
 
-@app_views.route('/states/<state_id>/cities', methods=['POST'], strict_slashes=False)
-def create_city(state_id):
-    """ creates a city under a specific state """
-    state = storage.get(State, state_id)
-    if state is None:
-        abort(404)
+@app_views.route('/cities', methods=['POST'], strict_slashes=False)
+def create_city():
+    """ creates a city """
     if not request.is_json:
         abort(400, description="Not a JSON")
     data = request.get_json(silent=True)
@@ -48,7 +44,6 @@ def create_city(state_id):
     if 'name' not in data:
         abort(400, description="Missing name")
     new_city = City(**data)
-    new_city.state_id = state.id
     new_city.save()
     return jsonify(new_city.to_dict()), 201
 
@@ -64,7 +59,7 @@ def update_city(city_id):
     data = request.get_json(silent=True)
     if data is None:
         abort(400, description="Not a JSON")
-    ignore_keys = ['id', 'state_id', 'created_at', 'updated_at']
+    ignore_keys = ['id', 'created_at', 'updated_at']
     for key, value in data.items():
         if key not in ignore_keys:
             setattr(city, key, value)
