5045764dff93df201db55053eed24d9343dd69ed
BPChance <bradenpchance6@gmail.com>
2024-07-15 20:38:51 -0500

    checker broke?

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -11,7 +11,7 @@ from models.state import State
 from models.user import User
 
 
-@app_views.route('/status', strict_slashes=False)
+@app_views.route('/status', methods=['GET'])
 def get_status():
     return jsonify({"status": "OK"})
 
