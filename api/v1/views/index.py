6b21b13d0569fe17dafa1e8834d8ca5a285b92c2
BPChance <bradenpchance6@gmail.com>
2024-07-15 21:20:51 -0500

    updated get_stats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -10,20 +10,30 @@ from models.review import Review
 from models.state import State
 from models.user import User
 
+model_classes = {"amenities": Amenity, "cities": City, "places": Place,
+           "reviews": Review, "states": State, "users": User}
 
 @app_views.route('/status', methods=['GET'])
 def get_status():
+    """ returns api status """
     return jsonify({"status": "OK"})
 
 
-@app_views.route('/stats')
+@app_views.route('/stats', strict_slashes=False, methods=['GET'])
 def get_stats():
-    stats = {
-        "amenities": storage.count("Amenity"),
-        "cities": storage.count("City"),
-        "places": storage.count("Place"),
-        "reviews": storage.count("Review"),
-        "states": storage.count("State"),
-        "users": storage.count("User")
-    }
-    return jsonify(stats)
+    """ return counts of different objects """
+    return_dict = {}
+    for object in model_classes:
+        object_dict = {object: storage.count(model_classes[object])}
+        return_dict.update(object_dict)
+    return jsonify(return_dict)
+
+    #stats = {
+     #   "amenities": storage.count("Amenity"),
+      #  "cities": storage.count("City"),
+       # "places": storage.count("Place"),
+        #"reviews": storage.count("Review"),
+        #"states": storage.count("State"),
+        #"users": storage.count("User")
+    #}
+    #return jsonify(stats)
