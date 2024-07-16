76db270379593ac0c3cd02a8b5a8f6bb4381c074
BPChance <bradenpchance6@gmail.com>
2024-07-16 00:29:01 +0000

    updated index get_stats method

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -11,20 +11,21 @@ from models.review import Review
 from models.state import State
 from models.user import User
 
+model_classes = {"amenities": Amenity, "cities": City,
+           "places": Place, "reviews": Review, "states": State, "users": User}
+
 
 @app_views.route('/status', strict_slashes=False)
 def get_status():
+    """ return api status """
     return jsonify({"status": "OK"})
 
 
-@app_views.route('/stats')
+@app_views.route('/stats', strict_slashes=False, methods=['GET'])
 def get_stats():
-    stats = {
-        "amenities": storage.count("Amenity"),
-        "cities": storage.count("City"),
-        "places": storage.count("Place"),
-        "reviews": storage.count("Review"),
-        "states": storage.count("State"),
-        "users": storage.count("User")
-    }
-    return jsonify(stats)
+    """ return count for the different objects """
+    return_dict = {}
+    for object in model_classes:
+        object_dict = {object: storage.count(model_classes[object])}
+        return_dict.update(object_dict)
+    return jsonify(return_dict)
